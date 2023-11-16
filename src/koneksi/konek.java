9cf11401ed9c38e7b69986ebf63ecb9f053b4876
Jessicha0204 <jessicha.alvina@gmail.com>
2023-11-16 10:41:16 +0700

    add t

--- src/koneksi/konek.java
+++ src/koneksi/konek.java
@@ -1,6 +1,7 @@
 package koneksi;
 
-import com.mysql.cj.jdbc.Driver;
+//import com.mysql.cj.jdbc.Driver;
+import com.mysql.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
